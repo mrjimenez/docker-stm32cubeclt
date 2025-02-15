68f546f709831dd2f8774cae5ec23dfbe3b64381
Marcelo Roberto Jimenez <marcelo.jimenez@gmail.com>
2025-02-15 09:20:34 -0300

    Use the full CLT version in the tag

--- create_image.sh
+++ create_image.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 
 REPOSITORY=mrjimenez123/stm32cubeclt
-TAG=17.0
+TAG=1.17.0
 
 # build docker
 # docker build --progress=plain \
@@ -9,4 +9,4 @@ docker build \
         -t "${REPOSITORY}:${TAG}" \
         .
 
-# run with "docker run --rm -it mrjimenez123/stm32cubeclt:17.0"
+# run with "docker run --rm -it mrjimenez123/stm32cubeclt:1.17.0"
